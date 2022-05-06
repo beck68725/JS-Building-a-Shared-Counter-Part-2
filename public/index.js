96519b8963d7f118b0bbf07613ae44da3f007106
Rebecca Coburn <rebeccacoburn@Rebeccas-iMac.local>
2022-05-06 15:40:40 -0500

    completed assignment

--- public/index.js
+++ public/index.js
@@ -4,6 +4,7 @@ async function main(){
     const decrementButton = document.querySelector('#decrement-button');
 
     const response = await fetch('http://localhost:9001/counter');
+       
 
     const result = await response.json();
     
@@ -12,11 +13,31 @@ async function main(){
     function increment(){
         countValue++;
         countContainer.textContent = countValue;
+
+        fetch('http://localhost:9001/counter', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                value: countValue
+            })
+        })
     }
 
     function decrement(){
         countValue--;
         countContainer.textContent = countValue;
+
+        fetch('http://localhost:9001/counter', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                value: countValue
+            })
+        })
     }
 
     incrementButton.addEventListener('click', increment);
